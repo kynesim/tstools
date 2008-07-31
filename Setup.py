83cb113b8cee4c668cbe544c3b5ba77d974cefe0
tibs <none@none>
2008-07-31 10:09:59 +0000

    Add license blocks to the Pyrex files (should have remembered earlier).
    --HG--
    extra : convert_revision : svn%3Aeff31bef-be4a-0410-a8fe-e47997df2690/trunk%4028

--- Setup.py
+++ Setup.py
@@ -1,5 +1,30 @@
 """Setup.py -- for building tstools Pyrex modules
 """
+
+# ***** BEGIN LICENSE BLOCK *****
+# Version: MPL 1.1
+# 
+# The contents of this file are subject to the Mozilla Public License Version
+# 1.1 (the "License"); you may not use this file except in compliance with
+# the License. You may obtain a copy of the License at
+# http://www.mozilla.org/MPL/
+# 
+# Software distributed under the License is distributed on an "AS IS" basis,
+# WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+# for the specific language governing rights and limitations under the
+# License.
+# 
+# The Original Code is the MPEG TS, PS and ES tools.
+# 
+# The Initial Developer of the Original Code is Amino Communications Ltd.
+# Portions created by the Initial Developer are Copyright (C) 2008
+# the Initial Developer. All Rights Reserved.
+# 
+# Contributor(s):
+#   Tibs (tibs@berlios.de)
+# 
+# ***** END LICENSE BLOCK *****
+
 from distutils.core import setup
 from Pyrex.Distutils.extension import Extension
 from Pyrex.Distutils import build_ext
--- tstools.pyx
+++ tstools.pyx
@@ -1,6 +1,30 @@
 """tstools.pyx -- Pyrex bindings for the TS tools
 """
 
+# ***** BEGIN LICENSE BLOCK *****
+# Version: MPL 1.1
+# 
+# The contents of this file are subject to the Mozilla Public License Version
+# 1.1 (the "License"); you may not use this file except in compliance with
+# the License. You may obtain a copy of the License at
+# http://www.mozilla.org/MPL/
+# 
+# Software distributed under the License is distributed on an "AS IS" basis,
+# WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+# for the specific language governing rights and limitations under the
+# License.
+# 
+# The Original Code is the MPEG TS, PS and ES tools.
+# 
+# The Initial Developer of the Original Code is Amino Communications Ltd.
+# Portions created by the Initial Developer are Copyright (C) 2008
+# the Initial Developer. All Rights Reserved.
+# 
+# Contributor(s):
+#   Tibs (tibs@berlios.de)
+# 
+# ***** END LICENSE BLOCK *****
+
 # If we're going to use definitions like this in more than one pyx file, we'll
 # need to define the shared types in a .pxd file and use cimport to import
 # them.
