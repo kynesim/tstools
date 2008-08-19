33f2feef012d405774817bc8beb0ab08b329c4c7
tibs <none@none>
2008-08-19 14:49:00 +0000

    Support an alternative location of test data, and also lose (notional) Python 2.2 support in the rundoctest script.
    --HG--
    extra : convert_revision : svn%3Aeff31bef-be4a-0410-a8fe-e47997df2690/trunk%4035

--- rundoctest.py
+++ rundoctest.py
@@ -30,23 +30,13 @@ def main():
     if not filename:
         filename = "test.txt"
 
-    try:
-        (failures,tests) = doctest.testfile(filename,verbose=verbose)
-        if failures > 0:
-            print doctest.DIVIDER
-    except:
-        # e.g., Python 2.2
-        tester = doctest.Tester(globs={},verbose=verbose)
-        f = open(filename)
-        (failures,tests) = tester.runstring(f.read(),filename)
-        f.close()
-        if failures > 0:
-            print "*"*65
+    (failures,tests) = doctest.testfile(filename,verbose=verbose)
 
     testword = "test"
     if tests != 1: testword = "tests"
     failword = "failure"
     if failures != 1: failword = "failures"
+    print
     print "File %s: %d %s, %d %s"%(filename,tests,testword,failures,failword)
 
 if __name__ == "__main__":
