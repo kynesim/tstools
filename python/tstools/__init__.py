685b3b14bdab0142071bf26788f2e19a1d6c4fcb
tibs <none@none>
2009-02-12 23:19:20 +0000

    Python wrapping: make "tstools" into a package (initially with tstools.pyx in it, but eventually to allow for multiple files).
    --HG--
    rename : python/tstools.pyx => python/tstools/tstools.pyx
    extra : convert_revision : svn%3Aeff31bef-be4a-0410-a8fe-e47997df2690/trunk%40109

--- python/tstools/__init__.py
+++ python/tstools/__init__.py
@@ -1,55 +1,42 @@
-"""Setup.py -- for building tstools Pyrex modules
+"""tstools -- a package of Pyrex bindings for the tstools
+
+This is being developed on a Mac, running OS X, and also tested on my Ubuntu
+system at work.
+
+I do not expect it to build (as it stands) on Windows, as it is making
+assumptions that may not follow thereon.
+
+It is my intent to worry about Windows after it works on the platforms that
+I can test most easily!
 """
 
 # ***** BEGIN LICENSE BLOCK *****
 # Version: MPL 1.1
-# 
+#
 # The contents of this file are subject to the Mozilla Public License Version
 # 1.1 (the "License"); you may not use this file except in compliance with
 # the License. You may obtain a copy of the License at
 # http://www.mozilla.org/MPL/
-# 
+#
 # Software distributed under the License is distributed on an "AS IS" basis,
 # WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
 # for the specific language governing rights and limitations under the
 # License.
-# 
+#
 # The Original Code is the MPEG TS, PS and ES tools.
-# 
+#
 # The Initial Developer of the Original Code is Amino Communications Ltd.
 # Portions created by the Initial Developer are Copyright (C) 2008
 # the Initial Developer. All Rights Reserved.
-# 
+#
 # Contributor(s):
 #   Tibs (tibs@berlios.de)
-# 
+#
 # ***** END LICENSE BLOCK *****
 
-from distutils.core import setup
-from Pyrex.Distutils.extension import Extension
-from Pyrex.Distutils import build_ext
+# For the moment, treat all entities in submodules as if they were at
+# the top level
 
-setup(
-  name = 'tstools',
-  ext_modules=[ 
-    Extension("tstools/tstools", ["tstools/tstools.pyx"],
-              include_dirs=['..'],
-              libraries=['tstools'],
-              library_dirs=['../lib'],
-              ),
-    ],
-  cmdclass = {'build_ext': build_ext}
-)
+__all__ = ["tstools"]
 
-#setup(
-#  name = 'tstools',
-#  ext_modules=[ 
-#    Extension("tstools", ["tstools.pyx"],
-#              include_dirs=['..'],
-#              libraries=['tstools'],
-#              library_dirs=['../lib'],
-#              ),
-#    ],
-#  cmdclass = {'build_ext': build_ext}
-#)
-
+from tstools import *
